8062bbb45052ec3d0b44a7406e1804cfd4306124
Pawnpug <popcorn.wennberg@gmail.com>
2025-05-04 14:36:38 +0200

    Tog bort edge och la till ny edge

--- ListGraph.java
+++ ListGraph.java
@@ -5,22 +5,4 @@ import java.util.Set;
 public class ListGraph implements Graph {
 
     private final Map<City, Set<Edge>> cities = new HashMap<>();
-
-    public void add(Node n) {
-
-    }
-
-    public String hej(){
-        return "då";
-        String a = "test terminal"}}
-
-    public void remove (Node n) {
-
-        public void niggooolooo() {
-            int a = 0;
-        }
-
-        String "hejsansistatestet!!!!" ;
-    }
-
 }
