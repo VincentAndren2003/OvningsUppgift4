b67788281dfc964e97c00795652c4b5c15dc0ce1
Kasper Scott <kasperscott@gmail.com>
2025-05-04 16:14:13 +0200

    List graph

--- ListGraph.java
+++ ListGraph.java
@@ -1,8 +1,75 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public class ListGraph implements Graph {
+public class ListGraph{
 
-    private final Map<City, Set<Edge>> cities = new HashMap<>();
+    private final Map<Node, List<Edge>> nodes = new HashMap<>();
+
+    public ListGraph() {
+
+    }
+
+    public void add(Node node) {
+        nodes.put(node, new ArrayList<>());
+    }
+
+    public void connect(Node from, Node to, String name, int weight) {
+        if (!nodes.containsKey(from) || !nodes.containsKey(to)) {
+            throw new NoSuchElementException("Node " + from + " and/or Node " + to + " not found");
+        }
+        Edge edgeTo = new Edge(to, name, weight);
+        Edge edgeFrom = new Edge(from, name, weight);
+        nodes.get(from).add(edgeTo);
+        nodes.get(to).add(edgeFrom);
+    }
+    public void remove (Node from, Node to, String name, int weight){
+
+    }
+    public void disconnect(Node from, Node to, String name, int weight) {
+        if (!nodes.containsKey(from) || !nodes.containsKey(to)) {
+            throw new NoSuchElementException("Node " + from + " and/or Node " + to + " not found");
+        }
+        nodes.get(from).removeIf(edge -> edge.getDestination().equals(to));
+        nodes.get(to).removeIf(edge -> edge.getDestination().equals(from));
+    }
+    public void setConnectionWeight(Node from, Node to, int weight) {
+        if (!nodes.containsKey(from) || !nodes.containsKey(to)) {
+            throw new NoSuchElementException("Node " + from + " and/or Node " + to + " not found");
+        }
+        //Exception if edge not excist
+        Edge edge = getEdgeBetween(from, to);
+        if (edge == null) {
+            throw new NoSuchElementException("Edge not found");
+        }
+        edge.setWeight(weight);
+    }
+
+    public void getNodes(){
+        //Make copy of list
+    }
+
+    public List<Edge> getEdgesFrom(Node from){
+        if (!nodes.containsKey(from)) {
+            throw new NoSuchElementException("Node " + from + " not found");
+        }
+        return nodes.get(from);
+
+        //Make copy of list asd
+    }
+
+    public Edge getEdgeBetween(Node from, Node to) {
+        if (!nodes.containsKey(from) || !nodes.containsKey(to)) {
+            throw new NoSuchElementException("Node " + from + " and/or Node " + to + " not found");
+        }
+        for (Edge edge : nodes.get(from)) {
+            if (edge.getDestination().equals(to)) {
+                return edge;
+            }
+        }
+        return null;
+    }
+
+    public boolean pathExists(Node from, Node to) {
+        return true;
+    }
+
 }
